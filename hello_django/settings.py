0bdc94639d8edd667ddc0be3c622b0dc679da7bd
Noah Gilmore <noah.w.gilmore@gmail.com>
2024-02-20 21:32:12 -0800

    Add herokuapp to allowed hosts

--- hello_django/settings.py
+++ hello_django/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='foo')
 
 DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'quiet-harbor-46242.herokuapp.com']
 
 
 # Application definition
